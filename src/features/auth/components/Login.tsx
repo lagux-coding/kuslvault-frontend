a8f9c32ea5c7f91b9101625829440710519d1a9a
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-03-22 15:45:24 +0700

    update new style auth page

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,13 +1,11 @@
 import { useContext } from "react";
 import { AuthContext } from "@/context/AuthProvider";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import { z } from "zod";
 import api from "@/config/axios";
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormControl,
@@ -17,11 +15,10 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   username: z.string().min(4, {
-    message: "Username must be at least 4 characters.",
+    message: "Username must be at least 6 characters.",
   }),
   password: z.string().min(6, {
     message: "Password must be at least 6 characters.",
@@ -65,7 +62,7 @@ const SignIn = () => {
   return (
     <Form {...form}>
       <div className="mb-4 flex flex-col space-y-2 text-left">
-        <h1 className="text-3xl font-semibold tracking-wide">Welcome Back</h1>
+        <h1 className="text-3xl tracking-wide text-white">Welcome Back</h1>
       </div>
 
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
@@ -74,11 +71,19 @@ const SignIn = () => {
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input autoComplete="username" {...field} />
+                <input
+                  type="text"
+                  autoComplete="username"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Username
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
@@ -88,11 +93,19 @@ const SignIn = () => {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input type="password" autoComplete="current-password" {...field} />
+                <input
+                  type="password"
+                  autoComplete="current-password"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Password
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
@@ -100,7 +113,7 @@ const SignIn = () => {
 
         {/* Remember me */}
         <div className="flex items-center justify-between">
-          <FormField
+          {/* <FormField
             control={form.control}
             name="remember"
             render={({ field }) => (
@@ -118,47 +131,53 @@ const SignIn = () => {
                 </div>
               </FormItem>
             )}
-          />
+          /> */}
 
           <Link
             to="/forgot-password"
             tabIndex={-1}
-            className="text-muted-foreground text-sm hover:opacity-75"
+            className="text-sm text-gray-300 hover:opacity-75"
           >
             Forgot Password?
           </Link>
         </div>
 
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button
+          type="submit"
+          className="w-full cursor-pointer bg-[#A118FE] transition-colors hover:bg-[#8906e0]"
+        >
           Login
         </Button>
       </form>
 
-      <div className="relative my-2">
+      <div className="relative my-8">
         <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t"></span>
+          <span className="w-full border-t border-gray-300"></span>
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background text-muted-foreground px-2">Or continue with</span>
+          <span className="rounded-lg bg-black px-4 text-white backdrop-blur-lg">Or</span>
         </div>
       </div>
 
       {/* Oauth 2 */}
-      <div className="flex items-center gap-2">
-        <Button variant="outline" className="w-1/2 cursor-pointer">
+      <div className="flex flex-col items-center gap-2">
+        <Button
+          variant="outline"
+          className="w-full cursor-pointer border-purple-500 hover:bg-purple-500"
+        >
           <img src="/svg/github.svg" alt="Github" className="h-4 w-4" /> Github
         </Button>
-        <Button variant="outline" className="w-1/2 cursor-pointer">
+        <Button
+          variant="outline"
+          className="w-full cursor-pointer border-purple-500 hover:bg-purple-500"
+        >
           <img src="/svg/gmail.svg" alt="Gmail" className="h-4 w-4" /> Gmail
         </Button>
       </div>
 
-      <div className="mt-2 text-center text-sm">
+      <div className="mt-2 text-center text-sm text-gray-400">
         Don't have an account?{" "}
-        <Link
-          to="/sign-up"
-          className="hover:text-primary underline underline-offset-4 hover:opacity-75"
-        >
+        <Link to="/sign-up" className="underline underline-offset-4 hover:opacity-75">
           Sign Up
         </Link>
       </div>
--- src/features/auth/components/SignUp.tsx
+++ src/features/auth/components/SignUp.tsx
@@ -14,18 +14,23 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-const formSchema = z.object({
-  username: z.string().min(4, {
-    message: "Username must be at least 4 characters.",
-  }),
-  email: z.string().email("Invalid email address."),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
-  }),
-  confirmPassword: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
-  }),
-});
+const formSchema = z
+  .object({
+    username: z.string().min(4, {
+      message: "Username must be at least 4 characters.",
+    }),
+    email: z.string().email("Invalid email address."),
+    password: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+    confirmPassword: z.string().min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
 
 const SignUp = () => {
   // 1. Define the form
@@ -49,7 +54,7 @@ const SignUp = () => {
   return (
     <Form {...form}>
       <div className="mb-4 flex flex-col space-y-2 text-left">
-        <h1 className="text-3xl font-semibold tracking-wide">Create your account</h1>
+        <h1 className="text-3xl font-semibold tracking-wide text-white">Create your account</h1>
       </div>
 
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
@@ -58,11 +63,18 @@ const SignUp = () => {
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input autoComplete="username" {...field} />
+                <input
+                  autoComplete="username"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Username
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
@@ -73,11 +85,19 @@ const SignUp = () => {
           control={form.control}
           name="email"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input {...field} />
+                <input
+                  type="email"
+                  autoComplete="email"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Email
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
@@ -88,11 +108,19 @@ const SignUp = () => {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input type="password" autoComplete="new-password" {...field} />
+                <input
+                  type="password"
+                  autoComplete="new-password"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Password
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
@@ -103,45 +131,59 @@ const SignUp = () => {
           control={form.control}
           name="confirmPassword"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirm Password</FormLabel>
+            <FormItem className="relative">
               <FormControl>
-                <Input type="password" autoComplete="new-password" {...field} />
+                <input
+                  type="password"
+                  autoComplete="new-password"
+                  {...field}
+                  className="peer block w-full appearance-none border-0 border-b-2 border-gray-500 bg-transparent px-0 py-3 text-sm text-white focus:border-violet-600 focus:ring-0 focus:outline-none dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+                  placeholder=" "
+                />
               </FormControl>
+              <FormLabel className="text-md absolute top-3 z-10 origin-[0] -translate-y-6 scale-75 transform text-gray-300 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-80 peer-focus:text-violet-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-violet-500">
+                Confirm Password
+              </FormLabel>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className="w-full cursor-pointer">
+        <Button
+          type="submit"
+          className="w-full cursor-pointer bg-[#A118FE] transition-colors hover:bg-[#8906e0]"
+        >
           Sign Up
         </Button>
       </form>
 
-      <div className="relative my-2">
+      <div className="relative my-8">
         <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t"></span>
+          <span className="w-full border-t border-gray-300"></span>
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background text-muted-foreground px-2">Or continue with</span>
+          <span className="rounded-lg bg-black px-4 text-white backdrop-blur-lg">Or</span>
         </div>
       </div>
 
       {/* Oauth 2 */}
-      <div className="flex items-center gap-2">
-        <Button variant="outline" className="w-1/2 cursor-pointer">
+      <div className="flex flex-col items-center gap-2">
+        <Button
+          variant="outline"
+          className="w-full cursor-pointer border-purple-500 hover:bg-purple-500"
+        >
           <img src="/svg/github.svg" alt="Github" className="h-4 w-4" /> Github
         </Button>
-        <Button variant="outline" className="w-1/2 cursor-pointer">
+        <Button
+          variant="outline"
+          className="w-full cursor-pointer border-purple-500 hover:bg-purple-500"
+        >
           <img src="/svg/gmail.svg" alt="Gmail" className="h-4 w-4" /> Gmail
         </Button>
       </div>
 
-      <div className="mt-2 text-center text-sm">
+      <div className="mt-2 text-center text-sm text-gray-400">
         Already have an account?{" "}
-        <Link
-          to="/login"
-          className="hover:text-primary underline underline-offset-4 hover:opacity-75"
-        >
+        <Link to="/login" className="underline underline-offset-4 hover:opacity-75">
           Login
         </Link>
       </div>
